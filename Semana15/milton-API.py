f45d83a84a9521e911e4416f392bc65442a1e486
Martín Gómez <123976183+Electromayonaise@users.noreply.github.com>
2024-05-15 09:43:11 -0500

    Modify shown API content

--- Semana15/milton-API.py
+++ Semana15/milton-API.py
@@ -11,4 +11,16 @@ dest="Palmira, CO"
 key = "S5QBdzNi6AtLvUfVdRTf5XRsrhn6LyYM" 
 url = main_api + urllib.parse.urlencode({"key":key, "from": orig, "to": dest})
 json_data=requests.get(url).json()
-print(json_data) #mostrar respuesta de peticion
+print("URL: " + (url))
+json_status = json_data["info"]["statuscode"]
+if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
+    print("=============================================")
+    print("Directions from " + (orig) + " to " + (dest))
+    print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
+    print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+    # print("Fuel Used (Gal): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78))) fuel used only available in USA
+    for each in json_data["route"]["legs"][0]["maneuvers"]:
+        print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+    print("=============================================")
+
